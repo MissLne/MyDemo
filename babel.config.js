f214004b2df6f300bf7b5668cebcf459f32d1095
MissLne <7351015+misslne@user.noreply.gitee.com>
2020-09-28 16:29:42 +0800

    demo

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,21 @@
+const prodPlugins = []
+if(process.env.NODE_ENV === 'production') {
+  prodPlugins.push('remove')
+}
+
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
+  "presets": [
+    "@vue/cli-plugin-babel/preset"
+  ],
+  "plugins": [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ],
+    ...prodPlugins,
+    '@babel/plugin-syntax-dynamic-import'
   ]
 }
